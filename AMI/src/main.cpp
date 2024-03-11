4e8a311ab53003156ed35256bf443e28ac435cd6
Pedro Romao <up202108660@g.uporto.pt>
2024-03-11 19:14:16 +0000

    adds Bounce 2 library implementation

--- AMI/src/main.cpp
+++ AMI/src/main.cpp
@@ -1,23 +1,33 @@
 #include <Arduino.h>
+#include <Bounce2.h>
 
 #define NUM_PINS 7
+#define DEBOUNCE_INTERVAL 5
+#define PRESSED_STATE LOW
 
 int inputPins[NUM_PINS] = {2, 3, 4, 5, 6, 7, 8};
 int outputPins[NUM_PINS] = {9, 10, 11, 12, 13, 14, 15};
+Bounce2::Button buttons[NUM_PINS];
 
-void setup() {
+void setup()
+{
   for (int i = 0; i < NUM_PINS; i++) {
-    pinMode(inputPins[i], INPUT);
     pinMode(outputPins[i], OUTPUT);
+    buttons[i].attach(inputPins[i], INPUT_PULLUP);
+    buttons[i].interval(DEBOUNCE_INTERVAL);
+    buttons[i].setPressedState(PRESSED_STATE);
   }
 }
 
-// LAST BUTTON CHECKED WINS
-void loop() {
+void loop()
+{
   int currentButtonPressed = -1;
 
-  for (int i = 0; i < NUM_PINS; i++) {
-    if (digitalRead(inputPins[i]) == LOW) {
+  for (int i = 0; i < NUM_PINS; i++)
+  {
+    buttons[i].update();
+    if (buttons[i].pressed())
+    {
       currentButtonPressed = i;
     }
   }
@@ -30,15 +40,3 @@ void loop() {
     digitalWrite(outputPins[currentButtonPressed], HIGH);
   }
 }
-
-// SIMPLEST WAY TO DO IT
-// void loop() {
-//   for (int i = 0; i < NUM_PINS; i++) {
-//     if (digitalRead(inputPins[i]) == LOW) {
-//       digitalWrite(outputPins[i], HIGH);
-//     }
-//     else {
-//       digitalWrite(outputPins[i], LOW);
-//     }
-//   }
-// }
