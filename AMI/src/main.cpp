e69a2be7217d607125bad94d70b45f229fed7b4b
Pedro Romao <up202108660@g.uporto.pt>
2024-03-27 13:08:07 +0000

    pin identifiers

--- AMI/src/main.cpp
+++ AMI/src/main.cpp
@@ -20,6 +20,11 @@ void setup()
   }
 }
 
+/**
+ * @brief AMI main loop: 
+ * Button gets picked and lights up the corresponding LED
+ * LAST BUTTON CHECKED WINS
+*/
 void loop() {
   int currentButtonPressed = -1;
 
