90c2dbc6f28402aab266773d3e1a34b2f254a471
Pedro Romao <up202108660@g.uporto.pt>
2024-04-08 17:36:50 +0100

    final

--- ami/src/main.cpp
+++ ami/src/main.cpp
@@ -1,25 +1,19 @@
 #include <Arduino.h>
-#include <Bounce2.h>
 
 #define NUM_PINS 7
-#define DEBOUNCE_INTERVAL 5
 #define PRESSED_STATE LOW
 
 // Use ATtiny pin identifiers
-// int inputPins[NUM_PINS] = {PA4, PA5, PA6, PA7, PB5, PB4, PB1};
-// int outputPins[NUM_PINS] = {PA2, PA1, PC3, PC2, PC1, PC0, PB0};
 int inputPins[NUM_PINS] = {0, 1, 2, 3, 4, 5, 8};
 int outputPins[NUM_PINS] = {15, 14, 13, 12, 11, 10, 9};
-Bounce2::Button buttons[NUM_PINS];
 
 void setup()
 {
   for (int i = 0; i < NUM_PINS; i++) {
     pinMode(outputPins[i], OUTPUT);
-    buttons[i].attach(inputPins[i], INPUT_PULLUP);
-    buttons[i].interval(DEBOUNCE_INTERVAL);
-    buttons[i].setPressedState(PRESSED_STATE);
+    pinMode(inputPins[i], INPUT_PULLUP);
   }
+  digitalWrite(outputPins[6], HIGH);
 }
 
 /**
@@ -28,12 +22,11 @@ void setup()
  * LAST BUTTON CHECKED WINS
 */
 void loop() {
-  int currentButtonPressed = -1;
+  int currentButtonPressed = 6;
 
   for (int i = 0; i < NUM_PINS; i++)
   {
-    buttons[i].update();
-    if (buttons[i].pressed())
+    if (digitalRead(inputPins[i]) == PRESSED_STATE)
     {
       currentButtonPressed = i;
     }
